499c88ae1ce6ad784ec804f6794e14ec68418d35
Luxiu123 <klt5466@163.com>
2022-10-26 16:51:19 +0800

    refactor: 添加 CheckSourcePathAndSavePath 方法

--- ImageProcessing/Proxy.cs
+++ ImageProcessing/Proxy.cs
@@ -11,20 +11,38 @@ internal static class Proxy {
         _ => throw new ArgumentException($"参数 {name} 错误"),
     };
 
-    private static void GaussianBlur(IConfiguration config) {
-        string sourcePath = config["sourcePath"];
-        string savePath = config["savePath"];
-        string radiusArg = config["radius"];
+    /// <summary>
+    /// 检查 sourcePath 和 savePath 是否合法
+    /// </summary>
+    /// <param name="sourcePath"></param>
+    /// <param name="savePath"></param>
+    /// <exception cref="FileNotFoundException"></exception>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    private static void CheckSourcePathAndSavePath(string sourcePath, string savePath) {
         var savePathDirectory = Path.GetDirectoryName(savePath);
 
         // 验证 sourcePath
         if (!File.Exists(sourcePath)) {
             throw new FileNotFoundException($"文件 '{sourcePath}' 找不到");
         }
-        // 验证 sourcePath
+        // 验证 savePath
         if (!Directory.Exists(savePathDirectory)) {
             throw new DirectoryNotFoundException($"目录 '{savePathDirectory}' 不存在");
         }
+    }
+
+    /// <summary>
+    /// 高斯模糊
+    /// </summary>
+    /// <param name="config"></param>
+    /// <exception cref="FileNotFoundException"></exception>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    private static void GaussianBlur(IConfiguration config) {
+        string sourcePath = config["sourcePath"];
+        string savePath = config["savePath"];
+        string radiusArg = config["radius"];
+
+        CheckSourcePathAndSavePath(sourcePath, savePath);
         // 解析 radiusArg
         if (!double.TryParse(radiusArg, out var radius)) {
             radius = Service.DefaultGaussianBlurRadius;
