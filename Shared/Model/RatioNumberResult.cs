74b24b38550a3dd03eb1ede0b19143ff969e927f
Luxiu123 <klt5466@163.com>
2023-05-11 14:56:45 +0800

    Add ParseRatioNumber extension method

---
+++ Shared/Model/RatioNumberResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Model;
+
+public readonly record struct RatioNumberResult {
+    public double Value { get; init; }
+    public bool HasValue { get; init; }
+    public bool IsRatio { get; init; }
+
+    public RatioNumberResult(double value, bool hasValue, bool isRatio) {
+        Value = value;
+        HasValue = hasValue;
+        IsRatio = isRatio;
+    }
+}
--- Shared/SharedHelper.cs
+++ Shared/SharedHelper.cs
@@ -1,8 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Shared.Model;
+using System.Text.RegularExpressions;
 
 namespace Shared;
 
-public static class SharedHelper {
+public static partial class SharedHelper {
+    /// <summary>
+    /// 非负数比例数参数正则，不包括前缀 '+'
+    /// </summary>
+    private static readonly Regex RatioNonNegativeNumberArgRegex = GetRatioNonNegativeNumberArgRegex();
+    /// <summary>
+    /// 比例数参数正则
+    /// </summary>
+    private static readonly Regex RatioNumberArgRegex = GetRatioNumberArgRegex();
+
     /// <summary>
     /// 检查参数是否有参数
     /// </summary>
@@ -22,7 +33,7 @@ public static class SharedHelper {
     /// </summary>
     /// <param name="args"></param>
     /// <returns></returns>
-    public static IConfiguration GetConfiguration(string[] args) => new ConfigurationBuilder().AddCommandLine(args).Build();
+    public static IConfiguration GetConfiguration(this string[] args) => new ConfigurationBuilder().AddCommandLine(args).Build();
 
     /// <summary>
     /// 是否启用调试模式
@@ -32,4 +43,36 @@ public static class SharedHelper {
     public static bool IsDebugMode(this IConfiguration configuration) {
         return configuration.GetSection("debug").Value?.ToLowerInvariant() == "true";
     }
+
+    /// <summary>
+    /// 解析比例参数
+    /// </summary>
+    /// <param name="arg"></param>
+    /// <param name="allowNegative">允许负数</param>
+    /// <returns></returns>
+    public static RatioNumberResult ParseRatioNumber(this string arg, bool allowNegative = false) {
+        var regex = allowNegative ? RatioNumberArgRegex : RatioNonNegativeNumberArgRegex;
+        var match = regex.Match(arg);
+        // Parse failed
+        if (!match.Success) {
+            return default;
+        }
+        return new(
+            double.Parse(match.Groups["number"].Value),
+            true,
+            arg.Contains('*')
+        );
+    }
+
+#if NET7_0_OR_GREATER
+    [GeneratedRegex(@"^(?<number>\d+(?:\.\d+)?)\*?$")]
+    private static partial Regex GetRatioNonNegativeNumberArgRegex();
+
+    [GeneratedRegex(@"^-?(?<number>\d+(?:\.\d+)?)\*?$")]
+    private static partial Regex GetRatioNumberArgRegex();
+#elif NET6_0_OR_GREATER
+    private static Regex GetRatioNonNegativeNumberArgRegex() => new(@"^(?<number>\d+(?:\.\d+)?)\*?$");
+
+    private static Regex GetRatioNumberArgRegex() => new(@"^-?(?<number>\d+(?:\.\d+)?)\*?$");
+#endif
 }
