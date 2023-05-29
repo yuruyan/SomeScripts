14f3b76fa10e2406fcf4eb6e8a421617a64327a8
Luxiu123 <klt5466@163.com>
2023-05-29 19:03:29 +0800

    Update visio services

--- VisioScripts/Services.cs
+++ VisioScripts/Services.cs
@@ -1,5 +1,52 @@
-﻿namespace VisioScripts;
+﻿using CommonTools.Utils;
+using Microsoft.Extensions.Configuration;
+using NLog;
+
+namespace VisioScripts;
 
 public static class Services {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
+    private static void BatchCreatingTextShapes(IConfiguration configuration) {
+        #region ArgumentNames
+        const string PathArgName = "path";
+        const string TextPathArgName = "textPath";
+        #endregion
+
+        #region Argument string value
+        var pathArg = configuration["PathArgName"];
+        var textPathArg = configuration["TextPathArgName"];
+        #endregion
+
+        #region Check arguments
+        // Path is empty
+        if (string.IsNullOrEmpty(pathArg)) {
+            Logger.Error($"Argument '{PathArgName}' cannot be empty");
+            return;
+        }
+        // TextPath is empty
+        if (string.IsNullOrEmpty(textPathArg)) {
+            Logger.Error($"Argument '{TextPathArgName}' cannot be empty");
+            return;
+        }
+        // Path not found
+        if (!File.Exists(pathArg)) {
+            Logger.Error($"File '{pathArg}' doesn't exist");
+            return;
+        }
+        // Path not found
+        if (!File.Exists(textPathArg)) {
+            Logger.Error($"File '{textPathArg}' doesn't exist");
+            return;
+        }
+        #endregion
+
+        VisioServices.BatchCreatingTextShapes(
+            pathArg,
+            File
+                .ReadAllText(textPathArg)
+                .ReplaceLineFeedWithLinuxStyle()
+                .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+        );
+    }
 }
