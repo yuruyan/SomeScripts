4e3bdbf20eb62985bc9b2993209aa2ea19455ecb
Luxiu123 <13467486215@163.com>
2023-12-05 16:51:00 +0800

    Add ApplicationProperty

---
+++ ApplicationProperty/Program.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationProperty;
+using NLog;
+using Shared;
+using System.Diagnostics;
+
+// 输入参数验证
+if (!SharedHelper.CheckArgs(args, Resource.Help)) {
+    return;
+}
+
+Logger Logger = LogManager.GetCurrentClassLogger();
+var Config = SharedHelper.GetConfiguration(args);
+string filepath = Config["filepath"];
+if (!File.Exists(filepath)) {
+    Logger.Error($"文件 '{nameof(filepath)}' 不存在！");
+    return;
+}
+// 获取文件属性
+try {
+    Console.WriteLine(FileVersionInfo.GetVersionInfo(filepath));
+} catch (Exception error) {
+    Logger.Error(error);
+    Environment.Exit(-1);
+}
