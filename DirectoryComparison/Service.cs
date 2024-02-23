38bc6116febd0648ab7939556103867684807ebb
Luxiu123 <klt5466@163.com>
2024-02-23 21:52:28 +0800

    Fix CompareDirectories

--- DirectoryComparison/Service.cs
+++ DirectoryComparison/Service.cs
@@ -31,30 +31,35 @@ internal static class Service {
     /// </summary>
     /// <param name="dir1"></param>
     /// <param name="dir2"></param>
-    /// <param name="currentRoot">当前父目录</param>
-    private static void CompareDirectories(DirectoryItem dir1, DirectoryItem dir2, string currentRoot) {
+    /// <param name="dir1Root">dir 父目录</param>
+    private static void CompareDirectories(DirectoryItem dir1, DirectoryItem dir2, string dirRoot) {
         // todo: 不区分大小写
         // 文件夹名称不一致
         if (dir1.Name != dir2.Name) {
-            Console.WriteLine(Path.Combine(currentRoot, dir2.Name));
+            Console.WriteLine(Path.Combine(dirRoot, dir2.Name));
             return;
         }
         // 比对文件
         foreach (var file in dir2.Files) {
+            // 新文件
             if (!dir1.Files.Contains(file)) {
-                Console.WriteLine(Path.Combine(currentRoot, file.Name));
+                Console.WriteLine(Path.Combine(dirRoot, dir2.Name, file.Name));
             }
         }
         // 比对文件夹
         var subDir1 = dir1.Directories.Select(item => item.Name).ToList();
-        var currentSubDirRoot = Path.Combine(currentRoot, dir2.Name);
+        var currentSubDirRoot = Path.Combine(dirRoot, dir2.Name);
         foreach (var dir in dir2.Directories) {
             // 新目录
             if (!subDir1.Contains(dir.Name)) {
                 Console.WriteLine(Path.Combine(currentSubDirRoot, dir.Name));
                 continue;
             }
-            CompareDirectories(dir1.Directories.First(item => item.Name == dir.Name), dir, dir2.Name);
+            CompareDirectories(
+                dir1.Directories.First(item => item.Name == dir.Name),
+                dir,
+                currentSubDirRoot
+            );
         }
     }
 
