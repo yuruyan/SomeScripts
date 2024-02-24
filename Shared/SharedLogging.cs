de9c5b19c3416c4ae44892ed70e7eb8a4375547c
yuruyan <luxiu.klt@foxmail.com>
2024-02-24 10:12:50 +0800

    Add SharedLogging

---
+++ Shared/SharedLogging.cs
@@ -0,0 +1,26 @@
+﻿using Karambolo.Extensions.Logging.File;
+using Microsoft.Extensions.Logging;
+
+namespace SharedHelper;
+
+public static class SharedLogging {
+    public static readonly ILogger FileLogger;
+    public static readonly ILogger Logger;
+
+    static SharedLogging() {
+        var factory = LoggerFactory.Create(builder => builder.AddConsole());
+        var fileFactory = LoggerFactory.Create(
+            builder => builder.AddConsole().AddFile(context => {
+                context.RootPath = Path.GetDirectoryName(Environment.ProcessPath);
+                context.Files = [new LogFileOptions() {
+                    Path = "app.log",
+                    MinLevel = new Dictionary<string, LogLevel> {
+                        {"Default", LogLevel.Warning}
+                    }
+                }];
+            })
+        );
+        Logger = factory.CreateLogger("Program");
+        FileLogger = fileFactory.CreateLogger("Program");
+    }
+}
