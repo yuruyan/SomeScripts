9cca475a1a2338bce3f94f8b08cedfa0571f74b4
yuruyan <luxiu.klt@foxmail.com>
2024-02-24 10:51:59 +0800

    Refactor DirectoryComparison

--- DirectoryComparison/Program.cs
+++ DirectoryComparison/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DirectoryComparison;
-using NLog;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Shared;
 
 // 输入参数验证
@@ -7,56 +8,47 @@ if (!SharedHelper.CheckArgs(args, Resource.Help)) {
     return;
 }
 
-Logger Logger = LogManager.GetCurrentClassLogger();
+var Logger = SharedLogging.Logger;
 var Config = SharedHelper.GetConfiguration(args);
 
 try {
     switch (args[0].ToLowerInvariant()) {
         case "savestructure":
-            SaveStructure(Config["Directory"], Config["SavePath"]);
+            SaveStructure(Config);
             break;
         case "comparedirectories":
-            CompareDirectories(Config["StructFile1"], Config["StructFile2"]);
+            CompareDirectories(Config);
             break;
         default:
-            Logger.Error("Invalid argument");
+            Logger.LogError("Invalid argument");
             break;
     }
 } catch (Exception error) {
-    Logger.Error(error);
+    Logger.LogError(error, "Program terminated");
     Environment.Exit(-1);
 }
 
-void CompareDirectories(string path1, string path2) {
-    if (string.IsNullOrEmpty(path1)) {
-        Logger.Error($"Argument StructFile1 cannot be empty");
+void CompareDirectories(IConfiguration config) {
+    string? file1 = config["StructFile1"], file2 = config["StructFile2"];
+    if (!SharedHelper.ValidateFileArgument(file1, "StructFile1")) {
         return;
     }
-    if (!File.Exists(path1)) {
-        Logger.Error($"File '{path1}' doesn't exist");
-        return;
-    }
-    if (string.IsNullOrEmpty(path2)) {
-        Logger.Error($"Argument StructFile2 cannot be empty");
-        return;
-    }
-    if (!File.Exists(path2)) {
-        Logger.Error($"File '{path2}' doesn't exist");
+    if (!SharedHelper.ValidateFileArgument(file2, "StructFile2")) {
         return;
     }
 
-    Service.CompareDirectories(path1, path2);
+    Service.CompareDirectories(file1!, file2!);
 }
 
-void SaveStructure(string directory, string savePath) {
-    if (string.IsNullOrEmpty(directory)) {
-        Logger.Error($"Argument Directory cannot be empty");
+void SaveStructure(IConfiguration config) {
+    string? dir = config["Directory"], file = config["SavePath"];
+    if (!SharedHelper.ValidateDirectoryArgument(dir, "Directory")) {
         return;
     }
-    if (!Directory.Exists(directory)) {
-        Logger.Error($"Directory '{directory}' doesn't exist");
+    if (!string.IsNullOrEmpty(file)) {
+        Logger.LogError("Argument 'SavePath' cannot be empty");
         return;
     }
 
-    Service.SaveStructure(directory, savePath);
+    Service.SaveStructure(dir!, file!);
 }
