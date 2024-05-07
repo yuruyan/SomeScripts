0cb1a89c86428922e9865700f5a50e182f75de60
yuruyan <luxiu.klt@foxmail.com>
2024-05-07 10:53:34 +0800

    Update ZSHConfiguration

--- ZSHConfiguration/main.go
+++ ZSHConfiguration/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"embed"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 
 	"github.com/fatih/color"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 //go:embed resources/*
 var embeddedFiles embed.FS
 
 func main() {
-	fmt.Println("Copying resources...")
+	fmt.Println("Coping resources...")
+	CopyResource("resources/ohmyzsh.zip", "ohmyzsh.zip")
+	Unzip("C:/Users/18804/ohmyzsh.zip", "ohmyzsh")
 }
 
 // PrintError 函数用于打印错误信息，并根据exit参数决定是否退出程序
@@ -26,7 +33,8 @@ func main() {
 // exit：是否退出程序
 func PrintError(err error, exit bool) {
 	red := color.New(color.FgRed).SprintFunc()
-	fmt.Fprintf(os.Stderr, "%s\n", red(err))
+	fmt.Fprintf(os.Stderr, "%s\n", red(err.Error()))
+	debug.PrintStack()
 	if exit {
 		os.Exit(1)
 	}
@@ -38,8 +46,8 @@ func PrintError(err error, exit bool) {
 //
 // resourcesPath：参数表示要复制的资源文件的路径
 //
-// dstPath：参数表示复制后的目标文件路径
-func CopyResource(resourcesPath string, dstPath string) {
+// savePath：参数表示要复制到的文件保存路径，相对于用户主目录
+func CopyResource(resourcesPath string, savePath string) {
 	// 打开嵌入的资源
 	file, err := embeddedFiles.Open(resourcesPath)
 	if err != nil {
@@ -51,7 +59,7 @@ func CopyResource(resourcesPath string, dstPath string) {
 	if err != nil {
 		PrintError(err, true)
 	}
-	dst := filepath.Join(home, resourcesPath)
+	dst := filepath.Join(home, savePath)
 	// 创建文件
 	out, err := os.Create(dst)
 	if err != nil {
@@ -65,4 +73,73 @@ func CopyResource(resourcesPath string, dstPath string) {
 	}
 	greenBg := color.New(color.FgGreen).SprintFunc()
 	fmt.Println(greenBg(fmt.Sprintf("%s copied to %s", resourcesPath, dst)))
+}
+
+// Unzip 函数用于解压缩ZIP文件到指定目录
+//
+// 参数：
+//
+// srcFile: ZIP文件路径
+//
+// saveDir: 解压后的文件保存目录
+func Unzip(srcFile string, saveDir string) {
+	// 打开ZIP文件
+	zipFile, err := zip.OpenReader(srcFile)
+	if err != nil {
+		PrintError(err, true)
+	}
+	defer zipFile.Close()
+
+	// 遍历ZIP文件中的文件
+	for _, file := range zipFile.File {
+		var decodeName string
+		if file.Flags == 0 {
+			//如果标致位是0  则是默认的本地编码   默认为gbk
+			i := bytes.NewReader([]byte(file.Name))
+			decoder := transform.NewReader(i, simplifiedchinese.GB18030.NewDecoder())
+			content, _ := io.ReadAll(decoder)
+			decodeName = string(content)
+		} else {
+			//如果标志为是 1 << 11也就是 2048  则是utf-8编码
+			decodeName = file.Name
+		}
+		// 创建目标文件路径
+		filePath := filepath.Join(saveDir, decodeName)
+
+		// 检查文件路径是否为目录
+		if file.FileInfo().IsDir() {
+			// 创建目录
+			os.MkdirAll(filePath, os.ModePerm)
+			continue
+		}
+
+		// 创建目标文件的父目录
+		dirPath := filepath.Dir(filePath)
+		os.MkdirAll(dirPath, os.ModePerm)
+
+		// 打开ZIP文件中的文件
+		zipFile, err := file.Open()
+		if err != nil {
+			PrintError(err, true)
+		}
+		defer zipFile.Close()
+
+		// 创建目标文件
+		outFile, err := os.Create(filePath)
+		if err != nil {
+			PrintError(err, true)
+		}
+		defer outFile.Close()
+
+		// 将ZIP文件中的文件内容复制到目标文件
+		_, err = io.Copy(outFile, zipFile)
+		if err != nil {
+			PrintError(err, true)
+		}
+	}
+
+	greenBg := color.New(color.FgGreen).SprintFunc()
+	srcFileAbs, _ := filepath.Abs(srcFile)
+	saveDirAbs, _ := filepath.Abs(saveDir)
+	fmt.Println(greenBg(fmt.Sprintf("'%s' unzipped to '%s'", srcFileAbs, saveDirAbs)))
 }
