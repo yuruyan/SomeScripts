c5b63bd8856bd807843f38bb5353e9632f5a5a82
yuruyan <luxiu.klt@foxmail.com>
2025-05-29 20:38:07 +0800

    Update DaemonProcess

--- DaemonProcess/Program.cs
+++ DaemonProcess/Program.cs
@@ -21,11 +21,18 @@ try {
                     CreateNoWindow = !info.ShowWindow,
                     UseShellExecute = info.ShowWindow,
                 };
+                var tmpFile = Path.GetFullPath(Path.GetRandomFileName() + ".bat");
                 if (info.EnvironmentVariables != null && info.EnvironmentVariables.Count > 0) {
+                    // 不显示cmd瞬间弹框
                     startInfo.UseShellExecute = false;
+                    var cmdContent = "@echo off\n";
+                    foreach (var env in info.EnvironmentVariables) {
+                        cmdContent += $"set {env.Key}=\"{env.Value}\"\n";
+                    }
+                    cmdContent += $"start \"{info.Path}\" \"{info.Path}\" {info.Args}";
+                    File.WriteAllText(tmpFile, cmdContent);
                     startInfo.FileName = "cmd.exe";
-                    var env = string.Join("&& set ", info.EnvironmentVariables.Select(x => $"{x.Key}={x.Value}"));
-                    startInfo.Arguments = $"/c \"set {env}&& start {info.Path} {info.Args}\"";
+                    startInfo.Arguments = $"/c {tmpFile}";
                 }
                 Process.Start(startInfo);
                 Console.WriteLine($"{DateTime.Now} Started {info.Path} with args {info.Args}");
